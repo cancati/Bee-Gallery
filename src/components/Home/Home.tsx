edcfed27e8fb5bfe4e6e70238fef5c86b3ecdcfe
mahmuutcaan <mahmuutcaan@gmail.com>
2021-05-26 13:49:58 +0300

    Clean Project

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -21,11 +21,11 @@ const HomePage = () => {
                     {photos.map((item ) => {return (<Card item={item}/>)})}
                 </div>
                     <div id={'Profil'} className={'position-sticky'} style={{width:'20%',minWidth:'20%',height:80 ,display:'flex',alignItems:'center',borderRadius:50,backgroundColor:'#f1a167',left:20,bottom:20}}>
-                        <Link to={'/AboutMe'}> <div  style={{width:70,minWidth:70,height:70 ,borderRadius:50,backgroundColor:'black',display:'flex',alignItems:'center',justifyContent:'center',left:20,marginLeft:10,bottom:20}}>
+                        <Link to={'/About'}> <div  style={{width:70,minWidth:70,height:70 ,borderRadius:50,backgroundColor:'black',display:'flex',alignItems:'center',justifyContent:'center',left:20,marginLeft:10,bottom:20}}>
                             <img src={Profiljpg} style={{width:64,height:64}} />
                         </div> </Link>
                         <div className={'font-weight-bold ml-3'}>
-                            <Link to={'/AboutMe'}>About Me ? </Link>
+                            <Link to={'/About'}>About Me ? </Link>
                         </div>
                     </div>
             </div>
@@ -42,7 +42,7 @@ const HomePage = () => {
                         <img src={Profiljpg} style={{width:64,height:64}} />
                     </div> </Link>
                     <div className={'font-weight-bold ml-3'}>
-                        <Link to={'/AboutMe'}>About Me ? </Link>
+                        <Link to={'/About'}>About Me ? </Link>
                     </div>
                 </div>
 
